efe92220609b156b849088aba3f3a16385c3989e
Ethan Duvall <eduvall23@turing.edu>
2024-02-01 09:51:22 -0700

    Add/baseline-html

---
+++ src/main.js
@@ -0,0 +1,21 @@
+var testFlower = {
+  version: "1",
+  flower: [
+    {
+      setName: "wandererTroupe",
+      position: "flower",
+      detailName: "Troupe's Dawnlight",
+      mainTag: { name: "lifeStatic", value: 3967 },
+      normalTags: [
+        { name: "defendStatic", value: 23 },
+        { name: "attackPercentage", value: 0.09300000000000001 },
+        { name: "recharge", value: 0.22 },
+        { name: "elementalMastery", value: 21 },
+      ],
+      omit: false,
+      id: 8,
+      level: 16,
+      star: 5,
+    },
+  ],
+};
